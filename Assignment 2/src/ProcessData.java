234dd6964925af20d27bead851ad40a29f9bf0dc
Sachin <lwxsac001@myuct..ac.za>
2020-03-13 23:09:23 +0200

    refinements

---
+++ Assignment 2/src/ProcessData.java
@@ -0,0 +1,12 @@
+public class ProcessData{
+    
+    public static void main(String[] args){
+        String[] data; 
+        for(int i = 0; i<data.length; i++)
+            File res = new File(name);
+            Scanner sc = new Scanner(res);
+            while(sc.hasNextLine()){
+                
+            } 
+    }
+}
